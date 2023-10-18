874184f11f0d018ca8beea0a7322ecf61e54c666
Francisco Carlos <fcarlosfs@hotmail.com>
2023-10-18 17:54:23 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -2,7 +2,14 @@ package com.nelioalves.cursomc.domain;
 
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity	
 public class Categoria {
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
 private Integer id;
 private String nome;
 public Categoria() {
