64e379560e13c0da5bb6b59809e41f2c66525886
Francisco Carlos <fcarlosfs@hotmail.com>
2023-10-18 13:16:45 -0300

    Testando a proimeira classe de domínio - Categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,26 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping
-	public String lista() {
-		return "Rest está funcionando";
+	public List<Categoria> lista() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List <Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 
 }
